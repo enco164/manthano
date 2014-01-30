7a15948745b94e47efa1a0b9b6217d84634831af
teva.biz@gmail.com <teva.biz@gmail.com@eef1bad6-0ef4-1560-8c28-5f0c28de649f>
2014-01-30 02:05:12 +0000

    message static

--- trunk/application/views/message_static.php
+++ trunk/application/views/message_static.php
@@ -1,1 +1,34 @@
-<p><?=$message?></p>
+<div class="jumbotron">
+	<div class="container">
+		<h1>
+			<ul class="list-inline">
+				<a href="index.php">
+
+					<img src="/assets/img/manthano-header.png" class="img-responsive " alt="Manthano">
+					<!--Manthano -->
+				</a>
+			</ul>
+		</h1>
+
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<p><?=$message?></p>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
